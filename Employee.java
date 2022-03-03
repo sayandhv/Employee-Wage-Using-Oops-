2e8cb9b15704c885c90d1cfd79560c0ee96ffa26
sayandhv <sayandhv160@gmail.com>
2022-03-03 16:17:03 +0530

    Employee Program

--- Employee.java
+++ Employee.java
@@ -2,19 +2,26 @@ package com.Bridgelabz;
 
 public class Employee {
 		 
-		  int IS_PRESENT = 1;
-		  int WAGE_PER_HR=20;
-		  int FULL_DAY_HR=8;  
-		  public boolean checkIsPresent(double empCheck){
-		        if(empCheck == IS_PRESENT)
-		            return true;
-		        else
-		            return false; 
-		    }
-
-		  public int calcDailyEmpWage(){
-		        int dailyEmpWage =  WAGE_PER_HR * FULL_DAY_HR;
-		        return  dailyEmpWage; 
-	}
-	
+	 int IS_PRESENT = 1;
+	  int WAGE_PER_HR = 20;
+	  int FULL_DAY_HR = 8;  
+	  int FULLTIME = 1;
+	  public boolean checkIsPresent(double empCheck){
+	        if(empCheck == IS_PRESENT)
+	            return true;
+	        else
+	            return false; 
+	  }
+	  public void calcDailyEmpWage(double emptype){
+	        int dailyEmpWage =  WAGE_PER_HR * FULL_DAY_HR;
+	        if(emptype == FULLTIME) {
+	        	System.out.println("Employee type : Full time");
+	        	System.out.println("Daily Employee wage :" +dailyEmpWage);
+	        }
+	        else {
+	        	System.out.println("Employee type : Part time");
+	        	System.out.println("Daily Employee Wage :" + (dailyEmpWage/2));
+	        }
+	  }
 }
+	
--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 package com.Bridgelabz;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		Employee emp = new Employee();
-        double empCheck = Math.floor (Math.random() * 10) % 2;
-        if(emp.checkIsPresent(empCheck)){
-            
-            System.out.println("Daily Employee Wage :" +  emp.calcDailyEmpWage());
-        }
-        else{
-            System.out.println("Employee is absent");
-        }
+    	double empCheck = Math.floor (Math.random() * 10) % 2;
+    	double empType = Math.floor (Math.random() * 10) % 2;
+	    if(emp.checkIsPresent(empCheck)){
+	    	
+	        emp.calcDailyEmpWage(empType);
+	       
+	    }
+    
+	    else {
+	    	
+	        System.out.println("Employee is absent");
+	    }
 	}
-
 }	
